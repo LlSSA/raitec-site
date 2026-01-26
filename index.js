f6c0aceca077fbe0a8729c54abbb99ac976f8a1c
Larissa Holanda <95726877+LlSSA@users.noreply.github.com>
2026-01-26 19:26:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
+// Libera acesso do front (CORS)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+// Rota principal
 app.get("/", (req, res) => {
-  res.send("API dos robôs rodando!");
+  res.send("Backend dos robôs está rodando!");
+});
+
+// Rota que o front vai usar
+app.get("/api", (req, res) => {
+  res.json({ mensagem: "Olá, os robôs estão conectados!" });
 });
 
 const PORT = process.env.PORT || 3000;
